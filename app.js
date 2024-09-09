3c261e2308a249ace3fa65f6486a66a60896c43d
Kapil Gwal <2909kapil2001@gmail.com>
2024-09-09 08:16:58 +0530

    completed the application

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const http = require("http");
 const socketio = require("socket.io");
+
+// Create an HTTP server and attach Socket.IO
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
+// Set EJS as the view engine
+app.set("view engine", "ejs");
 
-app.get("/", (req,res) => {
-    res.send("Hello World");
-})
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
 
-server.listen(3000);
+// Handle Socket.IO connections
+io.on("connection", (socket) => {
+    socket.on("send-location",function(data){
+        io.emit("recieved-location",{id : socket.id, ...data});
+    });
+    // console.log("A user connected");
+    socket.on("disconnect",function(){
+        io.emit("user-disconnected",socket.id);
+    })
+});
+
+// Define the route for the home page
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Start the server and listen on port 3000
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
